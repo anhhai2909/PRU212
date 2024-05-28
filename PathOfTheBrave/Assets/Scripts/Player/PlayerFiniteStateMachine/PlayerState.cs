2fcca8b003e136c433ef17e0466a5e05eeeb5690
kushinar09 <phongpd109@gmail.com>
2024-05-29 03:25:30 +0700

    update new input system

---
+++ PathOfTheBrave/Assets/Scripts/Player/PlayerFiniteStateMachine/PlayerState.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerState
+{
+    protected Core core;
+
+    protected Player player;
+    protected PlayerStateMachine stateMachine;
+    protected PlayerData playerData;
+
+    protected bool isAnimationFinished;
+    protected bool isExitingState;
+
+    protected float startTime;
+
+    public string animBoolName;
+
+    public PlayerState(Player player, PlayerStateMachine stateMachine, PlayerData playerData, string animBoolName)
+    {
+        this.player = player;
+        this.stateMachine = stateMachine;
+        this.playerData = playerData;
+        this.animBoolName = animBoolName;
+        core = player.Core;
+    }
+
+    public virtual void Enter()
+    {
+        DoChecks();
+        Debug.Log(animBoolName);
+        player.Anim.SetBool(animBoolName, true);
+        startTime = Time.time;
+        isAnimationFinished = false;
+        isExitingState = false;
+    }
+
+    public virtual void Exit()
+    {
+        player.Anim.SetBool(animBoolName, false);
+        isExitingState = true;
+    }
+
+    public virtual void LogicUpdate()
+    {
+
+    }
+
+    public virtual void PhysicsUpdate()
+    {
+        DoChecks();
+    }
+
+    public virtual void DoChecks() { }
+
+    public virtual void AnimationTrigger() { }
+
+    public virtual void AnimationFinishTrigger() => isAnimationFinished = true;
+    
+
+}
