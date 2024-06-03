e93fd28344a4385bdd948a01ae455c861f532324
TranHaiBang12 <133078395+TranHaiBang12@users.noreply.github.com>
2024-06-03 17:11:48 +0700

    update main menu, portal

---
+++ PathOfTheBrave/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float hp = 10;
+
+    public float coin = 10;
+
+    public float xSpawn = 0;
+
+    public float ySpawn = 0;
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Portal"))
+        {
+            transform.position = new Vector3(xSpawn, ySpawn, 0);
+            DataPersistenceManager instance = new DataPersistenceManager(hp, xSpawn, ySpawn, coin);
+            instance.SaveGame("");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1, LoadSceneMode.Single);
+            
+        }
+
+    }
+
+
+}
---
+++ PathOfTheBrave/Assets/Scripts/PortalScript.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PortalScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player;
+    public ParticleSystem portal;
+
+    private void Awake()
+    {
+        portal.loop = true;
+        portal.playOnAwake = false;
+        portal.enableEmission = false;
+        DontDestroyOnLoad(player);
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(portal.gameObject.transform.position.x - player.transform.position.x <= 10)
+        {
+            portal.Play();
+            portal.enableEmission = true;
+
+        }
+    
+    }
+
+
+
+
+
+
+}
