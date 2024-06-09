8967b4c0c6b7550886c5b601915160f67ad648c8
kushinar09 <phongpd109@gmail.com>
2024-06-10 02:45:32 +0700

    complete basic hitbox sword

--- PathOfTheBrave/Assets/Scripts/Enemies/CombatTestDummy.cs
+++ PathOfTheBrave/Assets/Scripts/Enemies/CombatTestDummy.cs
@@ -6,20 +6,26 @@ using UnityEngine;
 public class CombatTestDummy : MonoBehaviour, IDamageable
 {
     [SerializeField] private GameObject hitParticles;
-
+    [SerializeField] private float maxHealth;
+    private float health;
     private Animator anim;
+
 
     public void Damage(DamageData data)
     {
         Debug.Log(data.Amount + " Damage taken");
-
+        health -= data.Amount;
         Instantiate(hitParticles, transform.position, Quaternion.Euler(0.0f, 0.0f, Random.Range(0.0f, 360.0f)));
         anim.SetTrigger("damage");
-        //Destroy(gameObject);
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void Awake()
     {
         anim = GetComponent<Animator>();
+        health = maxHealth;
     }
 }
