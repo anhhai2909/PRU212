fe3d43b9e948800c207d5cb6e5147c41209f8007
kushinar09 <phongpd109@gmail.com>
2024-07-15 00:01:34 +0700

    update Invulnerability

--- PathOfTheBrave/Assets/Scripts/Core/CoreComponents/DamageReceiver.cs
+++ PathOfTheBrave/Assets/Scripts/Core/CoreComponents/DamageReceiver.cs
@@ -9,6 +9,8 @@ namespace CoreSystem
     {
         [SerializeField] private GameObject damageParticles;
         [SerializeField] private GameObject posParticles;
+        [SerializeField] private float invulnerabilityDuration = 1.0f;
+        private bool isInvulnerable = false;
         /*
          * Modifiers allows us to perform some custom logic on our DamageData before we apply it here. An example where this is being used is by the Block weapon component.
          * Blocking works by assigning a modifier during the active block window of the shield that reduces the amount of damage the player will take. For example: If a shield
@@ -22,7 +24,7 @@ namespace CoreSystem
 
         public void Damage(DamageData data)
         {
-            if (!core.isDashing)
+            if (!core.isDashing && !isInvulnerable)
             {
                 //print($"Damage Amount Before Modifiers: {data.Amount}");
 
@@ -38,8 +40,15 @@ namespace CoreSystem
 
                 stats.Health.Decrease(data.Amount);
                 particleManager.StartWithRandomRotation(damageParticles, posParticles);
+                StartCoroutine(InvulnerabilityCoroutine());
                 //Instantiate(damageParticles, posParticles.transform.position, Quaternion.Euler(0.0f, 0.0f, Random.Range(0.0f, 360.0f)));
             }
+        }
+        private IEnumerator InvulnerabilityCoroutine()
+        {
+            isInvulnerable = true;
+            yield return new WaitForSeconds(invulnerabilityDuration);
+            isInvulnerable = false;
         }
 
         protected override void Awake()
